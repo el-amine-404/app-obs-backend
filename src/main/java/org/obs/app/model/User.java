1a0737d37a84314fb3df5b14cf07f940f2a4d4b4
el-amine-404 <131032600+el-amine-404@users.noreply.github.com>
2024-07-02 14:56:07 +0100

    add User Model

---
+++ src/main/java/org/obs/app/model/User.java
@@ -0,0 +1,65 @@
+package org.obs.app.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users", schema = "obs_app")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
+    private Integer id;
+
+    @Column(name = "login", nullable = false, length = 50)
+    private String login;
+
+    @Column(name = "password", nullable = false, length = 50)
+    private String password;
+
+    @Column(name = "first_name", nullable = false, length = 50)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 50)
+    private String lastName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
