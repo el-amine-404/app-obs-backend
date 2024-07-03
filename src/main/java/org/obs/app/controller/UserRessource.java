01efbe3852a0aa838ab535ba4025947a4818049b
el-amine-404 <131032600+el-amine-404@users.noreply.github.com>
2024-07-03 10:35:23 +0100

    add getUser endpoint to get the user by id as JSON

--- src/main/java/org/obs/app/controller/UserRessource.java
+++ src/main/java/org/obs/app/controller/UserRessource.java
@@ -1,18 +1,31 @@
 package org.obs.app.controller;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.obs.app.model.User;
+import org.obs.app.service.UserService;
 
 @Path("/")
 public class UserRessource {
 
+    @Inject
+    UserService userService;
+
     @GET
     @Path("user")
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
     public String Hello() {
         return "Hello World !";
+    }
+
+    @GET
+    @Path("/user/{userId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public User getUser(Integer userId) {
+        return userService.getUser(userId);
     }
 
 }
