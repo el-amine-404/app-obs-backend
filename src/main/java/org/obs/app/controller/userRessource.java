05ddd2f7a1dc0dc276293521ff1bda1c61da5422
el-amine-404 <131032600+el-amine-404@users.noreply.github.com>
2024-07-03 09:37:59 +0100

    add userRessource

---
+++ src/main/java/org/obs/app/controller/userRessource.java
@@ -0,0 +1,18 @@
+package org.obs.app.controller;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/")
+public class userRessource {
+
+    @GET
+    @Path("user")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String Hello() {
+        return "Hello World !";
+    }
+
+}
