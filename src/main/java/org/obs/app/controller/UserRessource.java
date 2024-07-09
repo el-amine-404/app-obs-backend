dca00df70bb3cfe66e3ac70b65b3fd462d94f4ac
el-amine-404 <131032600+el-amine-404@users.noreply.github.com>
2024-07-09 11:09:55 +0100

    add CRUD endpoints, annotations and enhance the api URLs

--- src/main/java/org/obs/app/controller/UserRessource.java
+++ src/main/java/org/obs/app/controller/UserRessource.java
@@ -1,11 +1,9 @@
 package org.obs.app.controller;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import lombok.AllArgsConstructor;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
@@ -13,23 +11,23 @@ import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import org.obs.app.model.User;
 import org.obs.app.service.UserService;
 
-import java.util.List;
-
-@Path("/api/v1")
+@Path("/api/v1/user")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@AllArgsConstructor
 public class UserRessource {
-
-
+    
     private final UserService userService;
-
-    public UserRessource(UserService userService){
-        this.userService = userService;
-    }
-
+    
+    @Operation(summary = "Get all users", description = "Returns all users")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "Successfully retrieved")
+    })
     @GET
-    @Path("/hello")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello World !";
+    public Response getUsers() {
+        return Response
+                .ok(userService.getUsers())
+                .build();
     }
 
     @Operation(summary = "Get a user by id", description = "Returns a user as per the id")
@@ -37,25 +35,28 @@ public class UserRessource {
             @APIResponse(responseCode = "200", description = "Successfully retrieved")
     })
     @GET
-    @Path("/user/{userId}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getUser(@PathParam("userId") @Parameter(name="id", description="user id", example="1") Long userId) {
+    @Path("/{id}")
+    public Response getById(@PathParam("id") @Parameter(name="id", description="user id", example="1") Long userId) {
         return Response
                 .ok(userService.getUser(userId))
                 .build();
     }
 
-    @Operation(summary = "Get all users", description = "Returns all users")
-    @APIResponses(value = {
-            @APIResponse(responseCode = "200", description = "Successfully retrieved")
-    })
-    @GET
-    @Path("/users")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getUsers() {
-        return Response
-                .ok(userService.getUsers())
-                .build();
+    @POST
+    public Response create(User user) {
+        return Response.noContent().build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response update(@PathParam("id") long userId) {
+        return Response.noContent().build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Response replace(@PathParam("id") long userId, User user) {
+        return Response.noContent().build();
     }
 
 }
