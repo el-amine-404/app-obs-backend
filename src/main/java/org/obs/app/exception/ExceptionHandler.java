8c6c4cbca6a6139c5c11763403d662233550e4be
el-amine-404 <131032600+el-amine-404@users.noreply.github.com>
2024-07-22 14:56:27 +0100

    add logging for unexpected exceptions

--- src/main/java/org/obs/app/exception/ExceptionHandler.java
+++ src/main/java/org/obs/app/exception/ExceptionHandler.java
@@ -3,12 +3,16 @@ package org.obs.app.exception;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
+import org.jboss.logging.Logger;
+import org.jboss.logmanager.Level;
 
 import java.security.InvalidParameterException;
 import java.time.LocalDateTime;
 
 @Provider
 public class ExceptionHandler implements ExceptionMapper<Exception> {
+
+    private static final Logger LOG = Logger.getLogger(ExceptionHandler.class);
 
     @Override
     public Response toResponse(Exception e) {
@@ -23,6 +27,8 @@ public class ExceptionHandler implements ExceptionMapper<Exception> {
                     .entity(new ApiError(LocalDateTime.now(), Response.Status.BAD_REQUEST.getStatusCode(), Response.Status.BAD_REQUEST, e.getMessage()))
                     .build();
         }
+        LOG.error("An unexpected error occurred", e);
+        
         return Response
                 .serverError()
                 .build();
